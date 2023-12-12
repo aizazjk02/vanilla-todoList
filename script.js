c3afe379cc9f5bc96ee208231e9d8b02494b738c
Aizaz Kotwal <aizazk0309@protonmail.com>
2023-12-12 20:16:21 +0530

    add todo feature

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("connected!");
+const input = document.querySelector("#todo__input");
+const todoList = document.querySelector(".todo__list");
+
+function addTask() {
+  if (input.value === "") alert("Please write your todo!");
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    li.classList.add("todo__list__item");
+    todoList.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+
+  input.value = "";
+  saveData();
+}
+
+todoList.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", todoList.innerHTML);
+}
+
+function showTask() {
+  todoList.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
